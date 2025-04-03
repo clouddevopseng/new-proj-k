35a3f7e9f7c30fc8d4499bd271544ae46d819233
venkat <venkat@tcs.com>
2025-04-03 21:05:52 +0530

    Project

---
+++ src/main/java/samplecode/sampleart/SampleartApplication.java
@@ -0,0 +1,13 @@
+package samplecode.sampleart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/samplecode/sampleart/SampleartApplicationTests.java
@@ -0,0 +1,13 @@
+package samplecode.sampleart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
